caa3a634c6656060b1d3666faccb626dff0574d8
flyq <flyq951@gmail.com>
2020-06-08 18:13:50 +0800

    fix main

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
-#![feature(async_await)]
-
-use hyper::{Body, Client, Request, Response, Server, Uri, service::{make_service_fn, service_fn}};
+use hyper::{Body, Request, Response, Server, service::{make_service_fn, service_fn}};
 
 use std::net::SocketAddr;
 
 use cita_tool::{
-    client::basic::{Client, ClientExt},
+    client::basic::{Client as CitaClient, ClientExt},
     crypto::Encryption,
-    PrivateKey, TransactionOptions, UnverifiedTransaction,
+    PrivateKey, TransactionOptions,
 };
 use cita_types::U256;
 
@@ -16,15 +14,19 @@ pub const RPC_URL: &str = "http://101.132.38.100:1337";
 
 
 async fn serve_req(_req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
+    let encryption = Encryption::Secp256k1;
+    let priv_key: PrivateKey = PrivateKey::from_str("xxxxx", encryption)
+        .unwrap()
+        .into();
     let tx_options = TransactionOptions::new()
-                    .set_code("")
-                    .set_address("0xf26e01badf4c282edd8c8c14df84dae4a5855632")
-                    .set_value(Some(U256::from_str("1000000000000000000000").unwrap()));
-    let client = Client::new();
+        .set_code("0xfe")
+        .set_address("0xf26e01badf4c282edd8c8c14df84dae4a5855632")
+        .set_value(Some(U256::from_dec_str("1000000000000000000000").unwrap()));
+    let client = CitaClient::new();
     let mut client = client.set_uri(RPC_URL);
-    let client = client.set_private_key("xxx");
+    let client = client.set_private_key(&priv_key);
 
-    let rpc_response = client.send_raw_transaction(tx_options).unwrap();
+    let _rpc_response = client.send_raw_transaction(tx_options).unwrap();
     Ok(Response::new(Body::from("hello, world!")))
 }
 
