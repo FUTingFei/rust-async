6804d30153e2ee17cc5aa0251f6e8c09e51ed9a7
flyq <flyq951@gmail.com>
2020-06-10 17:02:46 +0800

    fix bugs

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,13 @@ use hyper::{Body, Request, Response, Server, service::{make_service_fn, service_
 use std::net::SocketAddr;
 
 use cita_tool::{
-    client::basic::{Client, ClientExt},
+    client::basic::{Client as CitaClient, ClientExt},
     crypto::Encryption,
     PrivateKey, TransactionOptions,
     JsonRpcParams, ParamsValue,
 };
 use cita_types::U256;
-use cita_cli::printer::Printer;
-
+use std::convert::From;
 
 
 pub const RPC_URL: &str = "http://xxx:1337";
@@ -26,15 +25,15 @@ async fn serve_req(_req: Request<Body>) -> Result<Response<Body>, hyper::Error> 
         .set_address("0xf26e01badf4c282edd8c8c14df84dae4a5855632")
         .set_value(Some(U256::from_dec_str("1000000000000000000000").unwrap()));
 
-    let client = Client::new();
+    let client = CitaClient::new();
     let mut client = client.set_uri(RPC_URL);
     let client = client.set_private_key(&priv_key);
 
     let number = 1000;
     let mut txs = Vec::with_capacity(number as usize);
     for _ in 0..number {
-        let tx = client.generate_transaction(tx_options).map_err(|err| format!("{}", err))?;
-        let byte_code = client.generate_sign_transaction(&tx).map_err(|err| format!("{}", err))?;
+        let tx = client.generate_transaction(tx_options).unwrap();
+        let byte_code = client.generate_sign_transaction(&tx).unwrap();
         let params = JsonRpcParams::new()
             .insert(
                 "method",
@@ -46,8 +45,8 @@ async fn serve_req(_req: Request<Body>) -> Result<Response<Body>, hyper::Error> 
             );
         txs.push(params);
     }
-    let result = client.send_request(txs.into_iter()).map_err(|err| format!("{}", err))?;
-    printer.println(&json!(result), true);
+    let _result = client.send_request(txs.into_iter());
+
 
     let _rpc_response = client.send_raw_transaction(tx_options).unwrap();
     Ok(Response::new(Body::from("hello, world!")))
